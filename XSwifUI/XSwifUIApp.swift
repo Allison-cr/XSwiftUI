7d5287335d48f17f7f39a712861ee54546ee2803
Allison <suprun-s@bk.ru>
2023-10-03 16:49:54 +0300

    Initial Commit

---
+++ XSwifUI/XSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XSwifUIApp.swift
+//  XSwifUI
+//
+//  Created by Alexander Suprun on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct XSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
